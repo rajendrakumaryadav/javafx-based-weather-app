c008dcacd758410c58896e8c18457eb83b21b672
Rajendra Kumar R Yadav <13816347+rajendrakumaryadav@users.noreply.github.com>
2021-09-24 11:51:25 +0530

    Final Finish!

---
+++ LearningSpring/src/test/java/in/co/dreamsdoor/learningspring/LearningSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package in.co.dreamsdoor.learningspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
